eaef17576e150c279fe04e9a16bcf4c8bc798675
SinaTZ <sinatofangsaz@gmail.com>
2026-09-14 02:12:16 +0330

    fix the deploy error

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  // Nginx terminates HTTPS before forwarding to react-router-serve over HTTP.
+  allowedActionOrigins: ["new.hut.ac.ir"],
 } satisfies Config;
